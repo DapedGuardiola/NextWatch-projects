0db90f84ed1beb814d44fb997a750a9ae59e0a00
DapedGuardiola <muhammaddavid81@gmail.com>
2026-05-17 16:44:53 +0700

    rewrite vector command : migrate fresh seed

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,10 +2,10 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-4 py-1.5 rounded-full bg-yellow-400/30 text-sm font-medium leading-5 text-yellow-900 focus:outline-none transition duration-150 ease-in-out'
+            ? 'inline-flex items-center px-4 py-1.5 rounded-full bg-yellow-400/30 text-sm font-medium leading-5 text-white focus:outline-none transition duration-150 ease-in-out'
             : 'inline-flex items-center px-4 py-1.5 rounded-full bg-black/30 text-sm font-medium leading-5 text-white-500 hover:bg-black/40 focus:outline-none transition duration-150 ease-in-out';
 @endphp
 <a {{ $attributes->merge(['class' => $classes]) }} 
-   style="{{ ($active ?? false) ? 'background-color: rgba(234, 179, 8, 0.3); color: #713f12; margin:2' : 'background-color: rgba(0, 0, 0, 0.3); color: #e5e7eb;' }}">
+   style="{{ ($active ?? false) ? 'background-color: rgba(234, 179, 8, 0.3); color: #ffffff; margin:2' : 'background-color: rgba(0, 0, 0, 0.3); color: #e5e7eb;' }}">
     {{ $slot }}
 </a>
--- resources/views/pages/actor-detail.blade.php
+++ resources/views/pages/actor-detail.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ $actorsData->image_url }}" alt="{{ $actorsData->name }}" class="absolute inset-0 object-cover object-center w-full h-full overflow-hidden">
         <div class="absolute backdrop-blur-3xl inset-0 bg-gradient-to-b from-transparent via-transparent from-0 via-[#212121]/20 via-25% via-[#212121]/50 via-50% via-[#212121]/70 via-75% to-[#212121] to-100%">
             <div class="ml-20 mt-12 font-bold text-2xl text-white">Movie played by {{ $actorsData->name }}</div>
-            <div class="shrink-0 grid grid-cols-5 max-w-[90%] place-items-center gap-8 mt-10 mx-auto">
+            <div class="grid grid-cols-5 max-w-[90%] place-items-center gap-8 mt-10 mx-auto mb-10">
                 @foreach($actorsData->actormovies as $actormovies)
                 <x-movie.movie-modal
                     :poster="$actormovies->movies->poster_url"
